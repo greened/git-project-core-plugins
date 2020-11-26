169eacafd72ea0e99d12d555fc8fa21b8692a638
David A. Greene <dag@cray.com>
2020-11-25 22:47:41 -0600

    [Packaging] Version bump
    Bump the version for callbacks changes.

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def readme():
         return f.read()
 
 setup(name='git_project_core_plugins',
-      version='0.0.2',
+      version='0.0.3',
       description='Core functionality for git-project',
       long_description=readme(),
       classifiers=[
