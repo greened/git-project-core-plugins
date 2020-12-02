90767a00ccfc6719514e76ec2144fff4abe6316a
David A. Greene <dag@cray.com>
2020-12-01 19:04:09 -0600

    [Packaging] Version bump
    Release 0.0.6.

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def readme():
         return f.read()
 
 setup(name='git_project_core_plugins',
-      version='0.0.5',
+      version='0.0.6',
       description='Core functionality for git-project',
       long_description=readme(),
       classifiers=[
