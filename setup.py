0997028cc9251f969573683078cb854f41f18b63
David Greene <dag@obbligato.org>
2021-03-17 15:40:43 -0500

    [Packaging] Version bump
    On to 0.0.13!

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def readme():
         return f.read()
 
 setup(name='git_project_core_plugins',
-      version='0.0.12',
+      version='0.0.13',
       description='Core functionality for git-project',
       long_description=readme(),
       classifiers=[
