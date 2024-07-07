dd0ee3270f1f70405c2902e9612063b03fadce29
David Greene <dag@basie.obbligato.org>
2024-07-07 15:34:22 -0500

    [packaging] Copnvert to hatch
    Build and distribute packages via hatch.

--- src/git_project_core_plugins/__init__.py
+++ src/git_project_core_plugins/__init__.py
@@ -1,6 +1,27 @@
-************************
+# SPDX-FileCopyrightText: 2020-present David A. Greene <dag@obbligato.org>
+
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+# Copyright 2024 David A. Greene
+
+# This file is part of git-project
+
+# git-project is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
+
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License along
+# with git-project. If not, see <https://www.gnu.org/licenses/>.
+
+"""
+========================
 git-project-core-plugins
-************************
+========================
 
 Plugins for `git-project <http://www.github.com/greened/git-project>`_
 
@@ -472,3 +493,14 @@ See also:
   artifact
   config
   run
+"""
+
+from .artifact import Artifact, ArtifactPlugin
+from .branch import BranchPlugin
+from .run import RunPlugin
+from .clone import ClonePlugin
+from .common import add_plugin_version_argument
+from .config import ConfigPlugin
+from .help import Help, HelpPlugin
+from .init import InitPlugin
+from .worktree import Worktree, WorktreePlugin
